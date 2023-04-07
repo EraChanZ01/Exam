dea1e4b35c17470544d2911c9ef5f160efd7b045
EraChanZ <ilya73960@gmail.com>
2023-04-07 23:39:24 +0300

    rewrote the models

--- server/src/models/Offer.js
+++ server/src/models/Offer.js
@@ -1,52 +1,61 @@
-
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-  const Offer = sequelize.define('Offers', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+  class Offer extends Model {
+    static associate(models) {
+      Offer.belongsTo(models.Users, {
+        foreignKey: 'userId', sourceKey: 'id'
+      });
+      Offer.belongsTo(models.Contests, {
+        foreignKey: 'contestId', sourceKey: 'id'
+      });
+      Offer.hasOne(models.Ratings, {
+        foreignKey: 'offerId', targetKey: 'id'
+      });
+    }
+  }
+  Offer.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
 
+      },
+      contestId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      text: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      fileName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      originalFileName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: 'pending',
+      },
     },
-    contestId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    text: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    fileName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    originalFileName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    status: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      defaultValue: 'pending',
-    },
-  },
-  {
-    timestamps: false,
-  });
+    {
+      sequelize,
+      modelName: "Offers",
+      timestamps: false
+    }
 
-  Offer.associate = function (models) {
-    Offer.belongsTo(models.User, { foreignKey: 'user_id', sourceKey: 'id' });
-  };
-
-  Offer.associate = function (models) {
-    Offer.belongsTo(models.Contest,
-      { foreignKey: 'contest_id', sourceKey: 'id' });
-  };
-
-  return Offer;
+  )
+  return Offer
 };
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -6,8 +6,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const configPath = env === 'production' ? path.join(__dirname, '..', '..', '..',
   'src/server/config/postgresConfig.json') : path.join(__dirname, '..',
-  '/config/postgresConfig.json');
-const config = require(configPath)[ env ];
+    '/config/postgresConfig.json');
+const config = require(configPath)[env];
 const db = {};
 
 const sequelize = new Sequelize(config.database, config.username,
@@ -17,36 +17,20 @@ fs
   .readdirSync(__dirname)
   .filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== basename) &&
-      (file.slice(-3) === '.js');
+      (file.slice(-3) === '.js') && (file.indexOf('.test.js') === -1);
   })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[ model.name ] = model;
+    console.log(db, 111111111111111111111111111)
+    db[model.name] = model;
   });
 
-db[ 'Contests' ].belongsTo(db[ 'Users' ],
-  { foreignKey: 'userId', sourceKey: 'id' });
-db[ 'Contests' ].hasMany(db[ 'Offers' ],
-  { foreignKey: 'contestId', targetKey: 'id' });
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
-db[ 'Users' ].hasMany(db[ 'Offers' ],
-  { foreignKey: 'userId', targetKey: 'id' });
-db[ 'Users' ].hasMany(db[ 'Contests' ],
-  { foreignKey: 'userId', targetKey: 'id' });
-db[ 'Users' ].hasMany(db[ 'Ratings' ],
-  { foreignKey: 'userId', targetKey: 'id' });
-
-db[ 'Offers' ].belongsTo(db[ 'Users' ],
-  { foreignKey: 'userId', sourceKey: 'id' });
-db[ 'Offers' ].belongsTo(db[ 'Contests' ],
-  { foreignKey: 'contestId', sourceKey: 'id' });
-db[ 'Offers' ].hasOne(db[ 'Ratings' ],
-  { foreignKey: 'offerId', targetKey: 'id' });
-
-db[ 'Ratings' ].belongsTo(db[ 'Users' ],
-  { foreignKey: 'userId', targetKey: 'id' });
-db[ 'Ratings' ].belongsTo(db[ 'Offers' ],
-  { foreignKey: 'offerId', targetKey: 'id' });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
