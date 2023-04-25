0357bd17d929482b2143456c21cab9c9bc38959b
EraChanZ <ilya73960@gmail.com>
2023-04-25 17:24:49 +0300

    create ButtonGroup component

---
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const ButtonGroup = () => {
+
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <div>
+                    <label>
+                        <input />
+                        <div></div>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <input />
+                        <div></div>
+                    </label>
+                </div>
+            </div>
+        </div>
+    )
+}
